181d33698c92093c37a1f36faeaa80ded835eeec
Yizi Gu <yg31@gpu.cs.rice.edu>
2020-10-31 17:01:59 -0500

    [custom-dyninst] added env setup file for gpu.cs.rice.edu

---
+++ env_setup_gpu_rice_custom_dyninst.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+module load cmake-3.17.0-gcc-7.4.0-urc7nrc
+module load gflags-2.1.2-gcc-7.4.0-o5opwkz 
+module load glog-0.3.5-gcc-7.4.0-mzyyzlv
+module load intel-tbb-2020.2-gcc-7.4.0-fnk5dga
+module load boost-1.72.0-gcc-7.4.0-vhqjhqk
+module load llvm-openmp-romp-mod-gcc-7.4.0-h7uu7gd
+module load dyninst-10.1.2-gcc-7.4.0-si3cm54
+module load gcc-7.4.0-gcc-4.8.5-hi4zkm2
+echo "modules loaded"
+
+
+
+
