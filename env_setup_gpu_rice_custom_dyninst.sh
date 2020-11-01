5a00ca4e0d40ce8694f6776eb615d3d0de517235
Yizi Gu <yg31@gpu.cs.rice.edu>
2020-11-01 17:10:05 -0600

    [custom-dyninst] updated modules

--- env_setup_gpu_rice_custom_dyninst.sh
+++ env_setup_gpu_rice_custom_dyninst.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-module load cmake-3.17.0-gcc-7.4.0-urc7nrc
-module load gflags-2.1.2-gcc-7.4.0-o5opwkz 
-module load glog-0.3.5-gcc-7.4.0-mzyyzlv
-module load intel-tbb-2020.2-gcc-7.4.0-fnk5dga
-module load boost-1.72.0-gcc-7.4.0-vhqjhqk
-module load llvm-openmp-romp-mod-gcc-7.4.0-h7uu7gd
-module load gcc-7.4.0-gcc-4.8.5-hi4zkm2
+module load cmake-3.18.4-gcc-7.4.0-sdl2v2e
+module load gflags-2.2.2-gcc-7.4.0-ff2dnrz
+module load glog-0.4.0-gcc-7.4.0-2cbvtl6
+module load intel-tbb-2020.3-gcc-7.4.0-7zu5c32
+module load boost-1.73.0-gcc-7.4.0-ckn6c2a
+module load llvm-openmp-romp-mod-gcc-7.4.0-l55kbym
+module load gcc-7.4.0-gcc-4.8.5-m7v2g7g
 echo "modules loaded"
 
 
